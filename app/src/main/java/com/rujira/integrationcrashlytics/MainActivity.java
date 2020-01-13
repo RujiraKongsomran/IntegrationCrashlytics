bfd08d46ef0303fcd2f4ff3a851993abb349a507
Rujira Kongsomran <biwberry@outlook.com>
2020-01-13 22:54:24 +0700

    Create bug for test in Crashlytics

--- app/src/main/java/com/rujira/integrationcrashlytics/MainActivity.java
+++ app/src/main/java/com/rujira/integrationcrashlytics/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] animals = {"cat", "dog", "pig"};
+        String selected = animals[6];
+
     }
 }
